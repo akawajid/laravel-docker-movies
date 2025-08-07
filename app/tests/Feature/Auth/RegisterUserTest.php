db594c1a3f8a97c886b36cbefa1e498c2c38a99f
Wajid R. <45237545+akawajid@users.noreply.github.com>
2025-08-07 06:33:57 +0400

    Added register new user test (#2)
    * Added register new user test

---
+++ app/tests/Feature/Auth/RegisterUserTest.php
@@ -0,0 +1,14 @@
+<?php
+
+it('register new user', function(string $name, string $email, string $password){
+    $response = $this->post('/register', [
+        'name' => $name,
+        'email' => $email,
+        'password' => $password,
+        'password_confirmation' => $password,
+    ]);
+
+    $this->assertAuthenticated();
+    $response->assertRedirect(route('dashboard', false));
+
+})->with('registerUsers');
